35f272bea4ed7618f5a660e61a93d7773363fd0c
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2024-01-10 13:53:35 +0100

    Email status on form button

--- forms.js
+++ forms.js
@@ -3,38 +3,36 @@
 "use strict";
 
 // EMAIL SENDER
- var sendEmail = async function (elementId, templateClass){
-        var loading = "false"
-         console.log(loading)
+ var sendEmail = async function (elementId, templateClass, submitButton){
+        submitButton.text("Sending Email...")
         var result =  await emailjs.sendForm('service_g8n60vk', templateClass, elementId)
-        loading = "true"
-         console.log(loading)
         if (result.status === 200 ) {
-            console.log('SUCCESS!', result.status, result.text);
-            loading = "false"
-             console.log(loading)
+            submitButton.text("Book Now")
+            alert("Email Sent!")
+       }else{
+       submitButton.text("Sending failed!")
        }
-       
-     
+      
 }
 
 // FORM DATA
    $( "form" ).on( "submit", function( event ) {
         event.preventDefault();
+
         var elementId = "#"+this.id
+        var submitButton = $(this).find('.submit-button');
         var templateClass ='template_ykrx50r'
+
         if (elementId === "#appointment"){
             templateClass = 'template_bzoeg6h'
         }
+
         var formData = $( this ).serializeArray()
         console.log(formData);
         var formValues ={}
         $.each(formData, function(index, field){
         formValues[this.name] = this.value
   })
-  sendEmail(elementId, templateClass)
- 
-     
-
+      sendEmail(elementId, templateClass, submitButton)
 } );
 })(window.jQuery)
